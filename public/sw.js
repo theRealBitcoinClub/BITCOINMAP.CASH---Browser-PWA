f6a47897d06da0618a45d455689558070c6f1049
TRBC <therealbitcoin.club@protonmail.com>
2019-05-21 16:15:25 +0200

    prettify & minify js/css/html

--- public/sw.js
+++ public/sw.js
@@ -36,15 +36,15 @@ workbox.routing.registerRoute(
 );
 
 workbox.precaching.precacheAndRoute([
-    { url: 'places8.json', revision: '383677' },
-    { url: 'jquery-3.3.1.min.js', revision: '383677' },
-    { url: 'taffy-min.js', revision: '383677' },
-    { url: 'markerclusterer.min.js', revision: '383677' },
-    { url: 'script.min.js', revision: '383677' },
-    { url: 'maptilerlayer.js', revision: '383677' },
-    { url: 'popper1.14.3.min.js', revision: '383677' },
-    { url: 'bootstrap4.1.1.min.js', revision: '383677' },
-    { url: 'bootstrap4.1.1.min.css', revision: '383677' },
-    { url: 'styles.css', revision: '383677' },
-    { url: '/index.html', revision: '383678' },
+    { url: 'places8.json', revision: '383679' },
+    { url: 'jquery-3.3.1.min.js', revision: '383679' },
+    { url: 'taffy-min.js', revision: '383679' },
+    { url: 'markerclusterer.min.js', revision: '383679' },
+    { url: 'script.min.js', revision: '383679' },
+    { url: 'maptilerlayer.js', revision: '383679' },
+    { url: 'popper1.14.3.min.js', revision: '383679' },
+    { url: 'bootstrap4.1.1.min.js', revision: '383679' },
+    { url: 'bootstrap4.1.1.min.css', revision: '383679' },
+    { url: 'styles.min.css', revision: '383679' },
+    { url: '/index.html', revision: '383679' },
 ]);
