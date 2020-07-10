5565ad32a91b1b109c6b97ae4e47033779d78f3e
TRBC <therealbitcoin.club@protonmail.com>
2020-07-10 07:18:17 -0400

    update for bitcoinplaces.epizy.com

--- public/bch-dash-btc/listcontent.php
+++ public/bch-dash-btc/listcontent.php
@@ -6,7 +6,184 @@ $json = json_decode($string, true);
 $firstRun = true;
 
 $discountText = array("10% discount on first BCH payment","20% discount on first BCH payment","Accepting Bitcoin payments soon","Trade Bitcoin here with 0.0% fee","DASH, BCH, BTC accepted here","Information from discoverdash.com");
-$tagText = array("Bitcoin","Events","Trading","Organic","Vegetarian","Vegan","Healthy","Burger","Sandwich","Muffin","Brownie","Cake","Cookie","Tiramisu","Pizza","Salad","Smoothie","Fruit","IceCream","Raw","Handbag","Cosmetic","Tattoo","Piercing","Souvenir","Hatha","Vinyasa","Massage","Upcycled","Coffee","NoGluten","Cocktails","Beer","Music","Projects","Electro","Rock","LiveDJ","Terrace","Seeds","Grinder","Papers","Advice","Calzone","Suppli","MakeUp","Gifts","Tapas","Copas","Piadina","Herbs","Grains","Fashion","Fair","Women","Drinks","TV","Retro","Color","BW","BTC","BCH","Online","Booking","HotDog","Fast","Kosher","Sushi","Moto","Coche","Tablet","Chicken","Rabbit","Potatoe","DASH","ETH","ATM","Club","Dance","TakeAway","Meditation","Wine","Champagne","Alcohol","Booze","Hookers","Girls","Gay","Party","English","BnB","Haircut","Nails","Beauty","Miso","Teriyaki","Rice","Seafood","Hostel","Fries","Fish","Chips","Italian","Karaoke"," x x x ","Battery","Wheels","Men","Pasta","Dessert","Starter","BBQ","Noodle","Korean","Market","Bread","Bakery","Cafe","Games","Snacks","Elegant","Piano","Brunch","Nachos","Lunch","Breakfast","HappyHour","LateNight","Mexican","Burrito","Tortilla","Indonesian","Sports","Pastry","Bistro","Soup","Tea","Onion","Steak","Shakes","Empanadas","Dinner","Sweet","Fried","Omelette","Gin","Donut","Delivery","Cups","Filter","Juice","Vietnamese","Pie","Unagi","Greek","Japanese","Tacos","Kombucha","Indian","Nan","Club","Liquor","Pool","Hotel","Pork","Ribs","Kava","Chai","Izzy","Matcha","CBD","Latte");
+$tagText = array('Spicy 🌶️',
+                     'Salty 🥨',
+                     'Sour 😜',
+                     'Organic 🐵',
+                     'Vegetarian 🥕',
+                     'Vegan 🐮',
+                     'Healthy 💓',
+                     'Burger 🍔',
+                     'Sandwich 🥪',
+                     'Muffin 🧁', //The muffin icon is invisible
+                     'Brownie 🥮', //Brownie is invisible too
+                     'Cake 🎂',
+                     'Cookie 🍪',
+                     'Arabic 🥙',
+                     'Pizza 🍕',
+                     'Salad 🥗',
+                     'Smoothie 🥤',
+                     'Fruit 🍓',
+                     'IceCream 🍦',
+                     'Raw 🥦',
+                     'Handbag 👜',
+                     'Cosmetic 💅',
+                     'Tattoo ♣',
+                     'Piercing 🌀',
+                     'Souvenir 🎁',
+                     'Hatha 🧘',
+                     'Vinyasa 🧘',
+                     'Massage 💆',
+                     'Upcycled 🌲',
+                     'Coffee ☕',
+                     'NoGluten 🌽',
+                     'Cocktails 🍹',
+                     'Beer 🍺',
+                     'Music 🎵',
+                     'Chinese 🍜',
+                     'Duck 🍱',
+                     'Rock 🎸',
+                     'LiveDJ 🎧',
+                     'Terrace ☀️',
+                     'Seeds 🌱',
+                     'Grinder 🍌',
+                     'Papers 🚬',
+                     'Advice 🌴',
+                     'Calzone 🥟',
+                     'Falafel 🥙',
+                     'MakeUp 🤡',
+                     'Gifts 🎁',
+                     'Tapas 🍠',
+                     'Copas 🍹',
+                     'Piadina 🌮',
+                     'Cheese 🧀',
+                     'Grains 🌾',
+                     'Fashion 👗',
+                     'Fair 🤗',
+                     'Women 👩',
+                     'Drinks 🍹',
+                     'TV 📺',
+                     'Retro 🦄',
+                     'Feta 🐐',
+                     'DASH Ð',
+                     'BTC ₿',
+                     'BCH ₿',
+                     'ANYPAY ₿️',
+                     'ETH ₿',
+                     'HotDog 🌭',
+                     'Fast ⏩',
+                     'Kosher 🦄',
+                     'Sushi 🍣',
+                     'Moto 🛵',
+                     'Coche 🚘',
+                     'GOCRYPTO ₿',
+                     'Chicken 🐔',
+                     'Rabbit 🐰',
+                     'Potato 🥔',
+                     'Kumpir 🥔',
+                     'Kebap 🐄',
+                     'ATM 🏦',
+                     'Gyros 🐖',
+                     'Coconut 🥥',
+                     'ToGo 📦',
+                     'Meditation 🧘',
+                     'Wine 🍷',
+                     'Champagne 🥂',
+                     'Alcohol 🍾',
+                     'Booze 🥃',
+                     'Pancakes 🥞', //You cant remove because we use fixed indexes, but replace with another string that is unlikely to be typed in by the user
+                     'Croissant 🥐',
+                     'Popcorn 🍿',
+                     'SoftIce 🍦',
+                     'Dango 🍡',
+                     'BnB 🛏️',
+                     'Haircut ✂️',
+                     'Candy 🍭',
+                     'Beauty 💅',
+                     'Miso 🍱',
+                     'Chocolate 🍫',
+                     'Rice 🍚',
+                     'Seafood 🦀',
+                     'Hostel 🛏️',
+                     'Fries 🍟',
+                     'Fish 🐟', //100
+                     'Chips 🍟',
+                     'Italian 🇮🇹',
+                     'Whiskey 🥃',
+                     ' - - - ', //This is number 104 the no tag indicator, currently not used //TODO hide this field from the suggestions
+                     'Bourbon 🥃', //105
+                     'Liquor 🥃',
+                     'Men ♂️',
+                     'Pasta 🍝',
+                     'Dessert 🍬', //109
+                     'Starter 🥠', //110
+                     'BBQ 🍗',
+                     'Noodle 🍜',
+                     'Korean 🥟',
+                     'Market 🧺', //invisible item
+                     'Bread 🥖',
+                     'Bakery 🥨',
+                     'Cafe ☕',
+                     'Games 🎮',
+                     'Snacks 🍿',
+                     'Elegant 🕴️',
+                     'Piano 🎹',
+                     'Brunch 🍱',
+                     'Nachos 🌽',
+                     'Lunch 🥡',
+                     'Breakfast 🥐',
+                     'HappyHour 🥳', //hidden item
+                     'LateNight 🌜',
+                     'Mexican 🇲🇽',
+                     'Burrito 🌯',
+                     'Tortilla 🌮',
+                     'Indonesian 🇮🇩',
+                     'Sports 🏆',
+                     'Pastry 🥧',
+                     'Bistro 🍲',
+                     'Soup 🥣',
+                     'Tea 🍵',
+                     'Onion',
+                     'Steak 🥩',
+                     'Shakes 🥤',
+                     'Empanadas 🥟',
+                     'Dinner 🍽️',
+                     'Sweet 🍭',
+                     'Fried 🍳',
+                     'Omelette 🥚',
+                     'Gin 🍸',
+                     'Donut 🍩',
+                     'Delivery 🚚',
+                     'Cups ☕',
+                     'Filter',
+                     'Juice 🍊',
+                     'Vietnamese 🇻🇳',
+                     'Pie 🥮', //invisible item
+                     'Unagi 🐡',
+                     'Greek 🇬🇷',
+                     'Japanese 🇯🇵',
+                     'Tacos 🌮',
+                     'Kombucha 🍵',
+                     'Indian 🇮🇳',
+                     'Nan 🥪',
+                     'Club 🎶',
+                     'Honey 🍯',
+                     'Pool 🎱',
+                     'Hotel 🏨',
+                     'Pork 🥓',
+                     'Ribs 🍖',
+                     'Kava 🍵',
+                     'Chai 🍵',
+                     'Izzy 🍵',
+                     'Matcha 🍵',
+                     'Oden 🍢',
+                     'Latte ☕',
+                     'DASHText Ð',
+                     'CoinTigo ₿',
+                     'CoinText ₿',
+                     'Salamantex ₿',
+                     'CryptoBuyer ₿',
+                     'XPay ₿');
 
 $filter = isset($_GET['category']) ? $_GET['category'] : '';
 $param_id = isset($_GET['id']) ? $_GET['id'] : '';
@@ -57,7 +234,7 @@ foreach ($json as $key => $value) {
         continue;
       }
 
-       $tags .= "<a href='https://bitcoinmap.cash/bch-dash-btc/?tag=" . $tagText[$s] . "'>" . $tagText[$s] . "</a> - ";
+       $tags .= "<a href='/?tag=" . $tagText[$s] . "'>" . $tagText[$s] . "</a> - ";
        array_push($tagsArray, $tagText[$s]);
     }
     $tags = substr($tags, 0, strlen($tags) - 2);
@@ -96,19 +273,19 @@ foreach ($json as $key => $value) {
     }
     $firstRun = false;
 
-    echo "<div class='piccontainer'><img width='640' height='480' alt='" . $name . "' class='pic lazy' data-src='https://therealbitcoin.club/img/app/$id.$imageType' /></div>";
-    echo "<h3 class='name'><a href='https://bitcoinmap.cash/bch-dash-btc/?id=$id'>$name</a></h3>";
+    echo "<div class='piccontainer'><img width='640' height='480' alt='" . $name . "' class='pic lazy' data-src='https://bitcoinmap.cash/img/app/$id.gif' /></div>";
+    echo "<h3 class='name'><a href='/?id=$id'>$name</a></h3>";
     echo "<div class='secondrow'><span class='reviews'>Reviews: $stars ($count)</span>";
     if (strlen($category) > 1)
-      echo "<a href='https://bitcoinmap.cash/bch-dash-btc/?category=$category'><img class='icon' alt='" . $category . "' src='img/icons/icon$type.png' /><a/>";
+      echo "<a href='/?category=$category'><img class='icon' alt='" . $category . "' src='img/icons/icon$type.png' /><a/>";
     else
       echo "<img class='icon'  alt='icon" . $type . "' src='img/icons/icon$type.png' />";
-    echo "<a class='location' href='https://bitcoinmap.cash/bch-dash-btc/?location=$splittedLocation[0]'>$splittedLocation[0]</a>";
-    echo ", <a class='location' href='https://bitcoinmap.cash/bch-dash-btc/?location=$splittedLocation[1]'>$splittedLocation[1]</a>";
-    echo ", <a class='location' href='https://bitcoinmap.cash/bch-dash-btc/?location=$splittedLocation[2]'>$splittedLocation[2]</a></div>";
+    echo "<a class='location' href='/?location=$splittedLocation[0]'>$splittedLocation[0]</a>";
+    echo ", <a class='location' href='/?location=$splittedLocation[1]'>$splittedLocation[1]</a>";
+    echo ", <a class='location' href='/?location=$splittedLocation[2]'>$splittedLocation[2]</a></div>";
     echo "<h4 class='discount'>$discountText[$discount]</h4>";
     echo "<h4 class='tags'>$tags</h4>";
-    echo "<div class='batschcontainer'><a href='https://therealbitcoin.club/$id'><img alt='Google Maps Badge' class='batsch' src='img/badges/google-maps-badge282x84.png' /></a>";
-    echo "<a href='https://therealbitcoin.club/android'><img class='batsch' alt='Google Play Badge' src='img/badges/google-play-badge282x84.png' /></a></div>";
+    echo "<div class='batschcontainer'><a href='https://realbitcoinclub.epizy.com/$id'><img alt='Google Maps Badge' class='batsch' src='google-maps-badge282x84.png' /></a>";
+    echo "<a href='https://bitcoinmap.cash/localbitcoinmap'><img class='batsch' alt='Google Play Badge' src='google-play-badge282x84.png' /></a></div>";
 }
 ?>
